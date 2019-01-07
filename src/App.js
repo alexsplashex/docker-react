0e7288930f7ef37b82e31a93c547f432af412eeb
alex <alex@pharosproduction.com>
2019-01-07 10:39:28 +0200

    change in a new branche

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Azazuli Vovanchik !!!
+            Are you ready for a bottle?
           </p>
           <a
             className="App-link"
